1423b2a74666c74ba3337dd486448f818f8cc647
RBEGamer <marcel.ochsendorf@gmail.com>
2016-04-04 20:50:26 +0200

    added shader loader empty files

---
+++ tiled_loader/tmx_shader_loader.cpp
@@ -0,0 +1,9 @@
+//
+//  tmx_shader_loader.cpp
+//  tiled_loader
+//
+//  Created by Marcel Ochsendorf on 04.04.16.
+//  Copyright © 2016 Marcel Ochsendorf. All rights reserved.
+//
+
+#include "tmx_shader_loader.hpp"
---
+++ tiled_loader/tmx_shader_loader.hpp
@@ -0,0 +1,14 @@
+//
+//  tmx_shader_loader.hpp
+//  tiled_loader
+//
+//  Created by Marcel Ochsendorf on 04.04.16.
+//  Copyright © 2016 Marcel Ochsendorf. All rights reserved.
+//
+
+#ifndef tmx_shader_loader_hpp
+#define tmx_shader_loader_hpp
+
+#include <stdio.h>
+
+#endif /* tmx_shader_loader_hpp */
